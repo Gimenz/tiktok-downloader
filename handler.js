de319d2fa11ab316ad0cf179e31e47f16369c639
Gimenz <agaaja187@gmail.com>
2022-11-24 03:07:52 +0700

    bangkit

--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 const { default: axios } = require('axios');
+const { createCipheriv } = require('node:crypto')
 const fs = require('fs');
 const ProgressBar = require('progress')
 const path = require('path')
@@ -7,6 +8,7 @@ const chalk = require('chalk');
 const delay = ms => new Promise((resolve) => setTimeout(resolve, ms))
 let cookieString = fs.readFileSync('cookie', 'utf-8')
 let cookie = convertCookie(cookieString)
+const crypto = require('crypto')
 global.task
 
 function convertCookie(cookies) {
@@ -16,6 +18,35 @@ function convertCookie(cookies) {
         return cookies
     }
 }
+
+// https://github.com/atharahmed/tiktok-private-api/blob/main/src/services/signer.service.ts
+function xttparams(params) {
+    // Encrypt query string using aes-128-cbc
+    const cipher = createCipheriv(
+        "aes-128-cbc",
+        "webapp1.0+202106",
+        "webapp1.0+202106"
+    );
+    return Buffer.concat([cipher.update(params), cipher.final()]).toString(
+        "base64"
+    );
+}
+
+const _defaultApiParams = {
+    aid: "1988",
+    count: 30,
+    secUid: null,
+    cursor: 0,
+    cookie_enabled: true,
+    screen_width: 0,
+    screen_height: 0,
+    browser_language: "",
+    browser_platform: "",
+    browser_name: "",
+    browser_version: "",
+    browser_online: "",
+    timezone_name: "Europe/London",
+};
 
 const headers = {
     'user-agent': 'com.zhiliaoapp.musically/2022405010 (Linux; U; Android 7.1.2; en; ASUS_Z01QD; Build/N2G48H;tt-ok/3.12.13.1)',
@@ -172,19 +203,36 @@ async function searchUser(username) {
 // new TikTok().searchUser('lailaindahb')
 // searchUser('lailaindahb')
 
-async function getVideoList(userId, count = 30, cursor = 0, secUid = 0) {
-    const query = {
-        id: userId,
-        aid: 1988,
-        count,
-        cursor,
-        secUid
-    }
-    const res = await axios.get(`https://m.tiktok.com/api/post/item_list`, { headers, params: query })
+async function getUserInfo(user) {
+    const res = await axios.get(`https://api.tiktokv.com/aweme/v1/multi/aweme/detail/?aweme_ids=%5B{${user}}%5D`, {
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+        }
+    })
     return res.data
 }
 
-// getVideoList('6847876230878020609').then(x => console.log(x.itemListData.length))
+
+async function getVideoList(secUid, count = 30, cursor = 0) {
+    const param = {
+        ..._defaultApiParams,
+        secUid: secUid,
+        cursor: cursor,
+        count: count,
+        is_encryption: 1,
+    }
+    const xTTParams = xttparams(new URLSearchParams(param).toString())
+    const res = await axios.get('https://www.tiktok.com/api/post/item_list/?aid=1988&app_language=en&app_name=tiktok_web&battery_info=1&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F107.0.0.0%20Safari%2F537.36%20Edg%2F107.0.1418.35&channel=tiktok_web&cookie_enabled=true&device_id=7002566096994190854&device_platform=web_pc&focus_state=false&from_page=user&history_len=3&is_fullscreen=false&is_page_visible=true&os=windows&priority_region=RO&referer=https%3A%2F%2Fexportcomments.com%2F&region=RO&root_referer=https%3A%2F%2Fexportcomments.com%2F&screen_height=1440&screen_width=2560&tz_name=Europe%2FBucharest&verifyFp=verify_lacphy8d_z2ux9idt_xdmu_4gKb_9nng_NNTTTvsFS8ao&webcast_language=en&msToken=7UfjxOYL5mVC8QFOKQRhmLR3pCjoxewuwxtfFIcPweqC05Q6C_qjW-5Ba6_fE5-fkZc0wkLSWaaesA4CZ0LAqRrXSL8b88jGvEjbZPwLIPnHeyQq6VifzyKf5oGCQNw_W4Xq12Q-8KCuyiKGLOw=&X-Bogus=DFSzswVL-XGANHVWS0OnS2XyYJUm', {
+        headers: {
+            ...headers,
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
+            "x-tt-params": xTTParams,
+        }
+    })
+    return res.data
+}
+
+// getVideoList('MS4wLjABAAAAPkIdksWtpSxQgc5NjgJIzN3mHuS7hVxaGq7VaNPEJSU').then(x => console.log(x))
 
 async function getDownloadLink(id) {
     const res = await axios.get('https://api2.musical.ly/aweme/v1/feed/?aweme_id=' + id, { headers })
--- index.js
+++ index.js
@@ -8,106 +8,113 @@ if (!fs.existsSync('cookie')) return console.log(chalk.red('[Error]'), 'Cookie N
 if (fs.readFileSync('cookie', 'utf-8') == '') return console.log(chalk.red('[Error]'), 'Cookie Not FOund! please set cookie first');
 
 (async () => {
-    console.clear();
-    console.log('[+]', chalk.hex('#6DD5FA')('TikTok Profile All Videos Downloader'), chalk.hex('#78ffd6')('[Without Watermark]'));
-    console.log('[+]', chalk.hex('#a8ff78')('Coded by masgimenz © 2022'));
-    console.log('[+]', '[ github.com/Gimenz ] |', chalk.hex('#4286f4')('[ fb.me/mg.ezpz ]\n'));
+    try {
+        console.clear();
+        console.log('[+]', chalk.hex('#6DD5FA')('TikTok Profile All Videos Downloader'), chalk.hex('#78ffd6')('[Without Watermark]'));
+        console.log('[+]', chalk.hex('#a8ff78')('Coded by masgimenz © 2022'));
+        console.log('[+]', '[ github.com/Gimenz ] |', chalk.hex('#4286f4')('[ fb.me/mg.ezpz ]\n'));
 
-    let correct = false
-    while (!correct) {
-        const username = await inquirer
-            .prompt([{
-                name: 'username',
-                message: 'Type a username/name : '
-            },])
-            .then(answers => {
-                return answers.username;
-            });
+        let correct = false
+        while (!correct) {
+            const username = await inquirer
+                .prompt([{
+                    name: 'username',
+                    message: 'Type a username/name : '
+                },])
+                .then(answers => {
+                    return answers.username;
+                });
 
-        const searchUsername = await searchUser(username)
-        // console.log(searchUsername[0]);
-        let listusername = searchUsername.map(x => `${x.user_info.nickname} | @${x.user_info.unique_id}`)
-        listusername.push('CANCEL')
+            const searchUsername = await searchUser(username)
+            // console.log(searchUsername[0]);
+            let listusername = searchUsername.map(x => `${x.user_info.nickname} | @${x.user_info.unique_id}`)
+            listusername.push('CANCEL')
 
-        let select = await inquirer.
-            prompt([{
-                type: 'rawlist',
-                choices: listusername,
-                name: 'selected',
-                message: 'Which Profile ?',
-            }]).then(answers => {
-                return answers.selected
-            })
+            let select = await inquirer.
+                prompt([{
+                    type: 'rawlist',
+                    choices: listusername,
+                    name: 'selected',
+                    message: 'Which Profile ?',
+                }]).then(answers => {
+                    return answers.selected
+                })
 
-        if (select == 'CANCEL') return console.log('[CANCELED]');
-        const selected = searchUsername[listusername.indexOf(select)]
+            if (select == 'CANCEL') return console.log('[CANCELED]');
+            const selected = searchUsername[listusername.indexOf(select)]
 
-        console.log(
-            `\n${chalk.hex('#56CCF2')('##### [ User Info ] #####')}\n` +
-            chalk.hex('#78ffd6')(
-                `Username : @${selected.user_info.unique_id}\n` +
-                `Nickname : ${selected.user_info.nickname}\n` +
-                `Post : ${selected.user_info.aweme_count}\n` +
-                `Private : ${selected.user_info.secret == 1 ? true : false}\n` +
-                `Bio : ${selected.user_info.signature}\n` +
-                `Followers : ${formatK(selected.user_info.follower_count)}\n` +
-                `Following : ${formatK(selected.user_info.following_count)}\n`)
-        )
+            console.log(
+                `\n${chalk.hex('#56CCF2')('##### [ User Info ] #####')}\n` +
+                chalk.hex('#78ffd6')(
+                    `Username : @${selected.user_info.unique_id}\n` +
+                    `Nickname : ${selected.user_info.nickname}\n` +
+                    `Post : ${selected.user_info.aweme_count}\n` +
+                    `Private : ${selected.user_info.secret == 1 ? true : false}\n` +
+                    `Bio : ${selected.user_info.signature}\n` +
+                    `Followers : ${formatK(selected.user_info.follower_count)}\n` +
+                    `Following : ${formatK(selected.user_info.following_count)}\n`)
+            )
 
-        const validate = await inquirer
-            .prompt([{
-                type: 'confirm',
-                name: 'value',
-                message: 'is the data correct?',
-            }])
-            .then(answers => {
-                return answers.value
-            })
+            // return console.log(selected.user_info);
 
-        if (validate) {
-            correct = true
-            let videoIds = []
-            console.log('Scraping All Video IDs');
-            let cursor = 0
-            let index = 1
-            let done = false
-            while (!done) {
-                const videoList = await getVideoList(selected.user_info.uid, 30, cursor, selected.user_info.sec_uid)
-                if (videoList !== undefined) {
-                    console.log('hasMore :', videoList.hasMore, '|', videoList.itemList.length, 'videos');
-                    cursor = videoList.cursor
-                    done = !videoList.hasMore
-                    for (let i = 0; i < videoList.itemList.length; i++) {
-                        videoIds.push({
-                            index: index,
-                            id: videoList.itemList[i].id
-                        })
-                        index++
+            const validate = await inquirer
+                .prompt([{
+                    type: 'confirm',
+                    name: 'value',
+                    message: 'is the data correct?',
+                }])
+                .then(answers => {
+                    return answers.value
+                })
+
+            if (validate) {
+                correct = true
+                let videoIds = []
+                console.log('Scraping All Video IDs');
+                let cursor = 0
+                let index = 1
+                let done = false
+                while (!done) {
+                    const videoList = await getVideoList(selected.user_info.sec_uid, 30, cursor)
+                    if (videoList !== undefined) {
+                        const hasMore = videoList.hasMore
+                        console.log('hasMore :', hasMore, '|', videoList.itemList.length, 'videos');
+                        cursor = videoList.cursor
+                        done = !hasMore
+                        for (let i = 0; i < videoList.itemList.length; i++) {
+                            videoIds.push({
+                                index: index,
+                                id: videoList.itemList[i].video.id
+                            })
+                            index++
+                        }
                     }
                 }
-            }
 
-            if (done) {
+                if (done) {
 
-                if (videoIds.length == 0) return console.log('No videos found!');
-                console.log(`\nFound ${videoIds.length} videos from @${selected.user_info.unique_id}\n`);
-                console.log('Start Downloading All Videos\n');
+                    if (videoIds.length == 0) return console.log('No videos found!');
+                    console.log(`\nFound ${videoIds.length} videos from @${selected.user_info.unique_id}\n`);
+                    console.log('Start Downloading All Videos\n');
 
-                let foldername = `@${selected.user_info.unique_id}`
-                if (!fs.existsSync('download')) fs.mkdirSync('download')
-                if (!fs.existsSync(`download/${foldername}`)) {
-                    fs.mkdirSync(`download/${foldername}`)
+                    let foldername = `@${selected.user_info.unique_id}`
+                    if (!fs.existsSync('download')) fs.mkdirSync('download')
+                    if (!fs.existsSync(`download/${foldername}`)) {
+                        fs.mkdirSync(`download/${foldername}`)
+                    }
+
+                    const getProfilePicture = await axios.get(selected.user_info.avatar_larger.url_list[1], { responseType: 'arraybuffer' })
+                    fs.writeFileSync(`./download/${foldername}/@${selected.user_info.unique_id}_profilePic.jpeg`, getProfilePicture.data)
+                    fs.writeFileSync(`./download/${foldername}/@${selected.user_info.unique_id}_info.json`, JSON.stringify(selected, null, 2))
+
+                    let dl = new Downloader(foldername)
+                    await dl.downloadFiles(videoIds)
+
                 }
-
-                const getProfilePicture = await axios.get(selected.user_info.avatar_larger.url_list[1], { responseType: 'arraybuffer' })
-                fs.writeFileSync(`./download/${foldername}/@${selected.user_info.unique_id}_profilePic.jpeg`, getProfilePicture.data)
-                fs.writeFileSync(`./download/${foldername}/@${selected.user_info.unique_id}_info.json`, JSON.stringify(selected, null, 2))
-
-                let dl = new Downloader(foldername)
-                await dl.downloadFiles(videoIds)
-
             }
-        }
 
+        }
+    } catch (error) {
+        console.log(error);
     }
 })()
